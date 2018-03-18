338df3133af2cedee6be0a9a2670642753bba387
Idriss LAHMIK <idriss-lahmik@hotmail.fr>
2018-03-18 19:07:44 +0100

    Add new feature parse_json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,30 +2,55 @@ require "json"
 
 class PagesController < ApplicationController
 
-  def index
-  end
+	def index
+	end
 
-  #TODO cut to small functions. Not Clean Code
-  def parse_gml
-    begin
-      gml_data = params["gml"].read
-      data = Hash.from_xml(gml_data)
-      data_label = data["graphml"]["graph"]["data"][12]
-      data_nodes = data["graphml"]["graph"]["node"]
-      data_edges = data["graphml"]["graph"]["edge"]
+	# TODO cut to small functions. Not Clean Code
+	def parse_gml
+		begin
+			gml_data = params["gml"].read
+			data = Hash.from_xml(gml_data)
+			data_label = data["graphml"]["graph"]["data"][12]
+			data_nodes = data["graphml"]["graph"]["node"]
+			data_edges = data["graphml"]["graph"]["edge"]
 
-      new_topology = Topology.create(name: data_label)
-      nodes = new_topology.nodes.create(data_nodes.map {|node| {name: node['id']}})
+			new_topology = Topology.create(name: data_label)
+			nodes = new_topology.nodes.create(data_nodes.map {|node| {name: node['id']}})
 
-      edges = []
-      data_edges.each do |edge|
-        source = nodes.find {|n| n.name == edge['source']}
-        target = nodes.find {|n| n.name == edge['target']}
-        edges << {source_node: source, target_node: target}
-      end
-      Link.create(edges)
-    rescue
-      redirect_to pages_index_path, :flash => { :error => "Parse Error" }
-    end
-  end
+			edges = []
+			data_edges.each do |edge|
+				source = nodes.find {|n| n.name == edge['source']}
+				target = nodes.find {|n| n.name == edge['target']}
+				edges << {source_node: source, target_node: target}
+			end
+			Link.create(edges)
+		rescue
+			redirect_to pages_index_path, :flash => { :error => "Parse Error" }
+		end
+	end
+
+	def parse_json
+		begin
+			gml_data = params["gml"].read
+			data = JSON.parse(gml_data)
+			data_label = data["graph"]["label"]
+			data_nodes = data["graph"]["nodes"]
+			data_edges = data["graph"]["edges"]
+
+			new_topology = Topology.create(name: data_label)
+			nodes = new_topology.nodes.create(data_nodes.map {|node| {name: node['id']}})
+
+			edges = []
+			data_edges.each do |edge|
+				source = nodes.find {|n| n.name == edge['source'].to_s}
+				target = nodes.find {|n| n.name == edge['target'].to_s}
+				edges << {source_node: source, target_node: target}
+			end
+			Link.create(edges)
+		rescue
+			redirect_to pages_index_path, :flash => { :error => "Parse Error" }
+		end
+		redirect_to pages_index_path, :flash => { :success => "Parse ok" }
+	end
+
 end
